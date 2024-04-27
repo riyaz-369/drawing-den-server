c302ee2559cabef7e6d336202dcac28b0684c97e
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-04-27 13:23:23 +0600

    data get and send using spesific id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -44,6 +44,13 @@ async function run() {
     app.get("/crafts", async (req, res) => {
       const cursor = artAndCraftCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artAndCraftCollection.findOne(query);
       res.send(result);
     });
 
