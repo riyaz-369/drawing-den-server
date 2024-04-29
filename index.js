91c02246772abed47ebeae7f4bdbb851e01c6d8a
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-04-29 13:01:51 +0600

    get specific subcategory

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
-// middleware
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
 app.use(cors());
 app.use(express.json());
 
@@ -28,9 +22,14 @@ async function run() {
   try {
     // await client.connect();
     const usersCollections = client.db("usersDB").collection("users");
+
     const artAndCraftCollection = client
       .db("artAndCraftDB")
       .collection("art and crafts");
+
+    const artAndCraftCategoryCollection = client
+      .db("artAndCraftCategoryDB")
+      .collection("category");
 
     app.get("/users", async (req, res) => {
       const cursor = usersCollections.find();
@@ -45,7 +44,6 @@ async function run() {
     });
 
     // art and craft related APIs
-
     app.get("/crafts", async (req, res) => {
       const cursor = artAndCraftCollection.find();
       const result = await cursor.toArray();
@@ -59,9 +57,42 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/crafts-email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await artAndCraftCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/crafts", async (req, res) => {
       const crafts = req.body;
       const result = await artAndCraftCollection.insertOne(crafts);
+      res.send(result);
+    });
+
+    app.put("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const formUpdatedData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          photo: formUpdatedData.photo,
+          itemName: formUpdatedData.itemName,
+          subCategory: formUpdatedData.subCategory,
+          details: formUpdatedData.details,
+          price: formUpdatedData.price,
+          rating: formUpdatedData.rating,
+          customization: formUpdatedData.customization,
+          processTime: formUpdatedData.processTime,
+          stock: formUpdatedData.stock,
+        },
+      };
+      const result = await artAndCraftCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
     });
 
@@ -69,6 +100,27 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await artAndCraftCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // category related APIs
+    app.get("/category", async (req, res) => {
+      const cursor = artAndCraftCategoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artAndCraftCategoryCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/craft-subCategory/:subCategory", async (req, res) => {
+      const subCategoryName = req.params.subCategory;
+      const query = { subCategory: subCategoryName };
+      const result = await artAndCraftCollection.find(query).toArray();
       res.send(result);
     });
 
